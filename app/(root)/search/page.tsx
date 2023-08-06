015f8faf0d48adc5aea5a3a45da2e5550345ed10
namiqmamedov <namigmm@code.edu.az>
2023-08-07 03:19:06 +0400

    Created search page

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import ProfileHeader from "@/components/shared/ProfileHeader";
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs"
+import {redirect} from 'next/navigation'
 
-const page = () => {
+import { profileTabs } from "@/constants";
+import Image from "next/image";
+import ThreadsTab from "@/components/shared/ThreadsTab";
+import { Fragment } from "react";
+import UserCard from "@/components/cards/UserCard";
+
+const Page = async () => {
+    const user = await currentUser();
+
+    if(!user) return null;
+
+    const userInfo = await fetchUser(user.id);
+
+    if(!userInfo?.onboarded) redirect('/onboarding');
+
+    // Fetch users
+    const result = await fetchUsers({
+        userId: user.id,
+        searchString: '',
+        pageNumber: 1,
+        pageSize: 25
+    })
+    
   return (
-    <div>page</div>
+    <section>
+        <h1 className="head-text mb-10">
+            Search
+        </h1>
+
+        {/* Search bar */}
+    
+        <div className="mt-14 flex flex-col gap-9">
+            {result.users.length === 0 ? (
+                <p className="no-result">No users</p>
+            ): (
+                <Fragment>
+                    {result.users.map((person) => (
+                        <UserCard
+                        key={person.id}
+                        id={person.id}
+                        name={person.name}
+                        username={person.username}
+                        imgUrl={person.image}
+                        personType='user'
+                    />
+                   ))}
+                </Fragment>
+            )}
+        </div>
+    </section>
   )
 }
 
-export default page
+export default Page

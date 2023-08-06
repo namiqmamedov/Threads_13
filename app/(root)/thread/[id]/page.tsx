ed0de879cebcad7f1b22b27be08ec9946711c729
namiqmamedov <namigmm@code.edu.az>
2023-08-06 22:20:56 +0400

    Updated thread detail page

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -35,7 +35,7 @@ const Page =  async ({params} : {params: {id: string}}) => {
         <div className="mt-7">
             <Comment
               threadId={thread.id}
-              currentUserImg={user.imageUrl}
+              currentUserImg={userInfo.image}
               currentUserId={JSON.stringify(userInfo._id)}
             />
         </div>
@@ -43,16 +43,16 @@ const Page =  async ({params} : {params: {id: string}}) => {
         <div className="mt-10">
           {thread.children.map((childItem: any) => (
                <ThreadCard
-               key={childItem._id}
-               id={childItem._id}
-               currentUserId={childItem?.id || ""}
-               parentId={childItem.parentId}
-               content={childItem.text}
-               author={childItem.author}
-               community={childItem.community}
-               createdAt={childItem.createdAt}
-               comments={childItem.children}
-               isComment
+                  key={childItem._id}
+                  id={childItem._id}
+                  currentUserId={childItem?.id || ""}
+                  parentId={childItem.parentId}
+                  content={childItem.text}
+                  author={childItem.author}
+                  community={childItem.community}
+                  createdAt={childItem.createdAt}
+                  comments={childItem.children}
+                  isComment
               />
           ))}
         </div>

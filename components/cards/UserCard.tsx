ac3688377dfe5af65c65d851f911b672828e1ed6
namiqmamedov <namigmm@code.edu.az>
2023-08-07 16:56:14 +0400

    Updated activity page

--- components/cards/UserCard.tsx
+++ components/cards/UserCard.tsx
@@ -1,4 +1,8 @@
+"use client"
+
 import Image from "next/image"
+import { Button } from "../ui/button"
+import { useRouter } from "next/navigation"
 
 interface Props {
     id: string,
@@ -9,6 +13,8 @@ interface Props {
 }
 
 const UserCard = ({id,name,username,imgUrl,personType}: Props) => {
+    const router = useRouter();
+
   return (
     <article className="user-card">
         <div className="user-card_avatar">
@@ -27,6 +33,10 @@ const UserCard = ({id,name,username,imgUrl,personType}: Props) => {
                 <p className="text-small-medium text-gray-1">@{username}</p>
             </div>
         </div>
+
+        <Button className="user-card_btn" onClick={() => router.push(`/profile/${id}`)}>
+            View
+        </Button>
     </article>
   )
 }
